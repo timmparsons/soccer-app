2495e8f2b31c9ca7ce7187e80bda45c47622ac01
Tim Parsons <timmparsons85@gmail.com>
2020-06-16 13:13:06 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+
+function App() {
+  return (
+    <BrowserRouter >
+      <div className="App">
+        <Navbar />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
