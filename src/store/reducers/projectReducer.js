6b9a3674cf2628c6fd4bc8fcbf3ba73f7f7a801d
Tim Parsons <timmparsons85@gmail.com>
2020-06-22 22:09:42 -0700

    added thunk

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,5 +1,9 @@
 const initState = {
-
+  projects: [
+    {id: '1', title: 'go to the store', content: 'random text'},
+    {id: '2', title: 'go for a run', content: 'random text'},
+    {id: '3', title: 'take a drive', content: 'random text'}
+  ]
 }
 
 const projectReducer = (state = initState, action ) => {
