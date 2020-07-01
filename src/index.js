6c06c7ae6c899d0e431667cfe77c380e8f76bde4
Tim Parsons <timmparsons85@gmail.com>
2020-07-01 08:29:21 -0700

    added firebase packages

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { getFirestore } from 'redux-firestore';
+import { getFirebase } from 'react-redux-firebase';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, 
+  compose(applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore}))
+));
 
 ReactDOM.render(
   <Provider store={store}>
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,5 @@
 export const createProject = (project) => {
-  return (dispatch, getState) => {
+  return (dispatch, getState, { getFirebase, getFireStore }) => {
     dispatch( { type: 'CREATE_PROJECT', project });
   }
 }
